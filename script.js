eacba84cf0598104d7aaea76beeea86d82b9538f
Emily McBride <henandhiveohio@gmail.com>
2023-02-17 11:09:38 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const displayCatFact = document.getElementById('cat-fact');
+
+const openModal = function () {
+  fetch('https://catfact.ninja/fact?max_length=140')
+    .then(response => response.json())
+    .then(data => {
+      displayCatFact.innerHTML = data.fact;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
